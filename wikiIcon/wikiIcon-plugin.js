f26ca4ac3f3888a41bba4f4b160ade16fa65e7a2
shahrooz <shahroozsanei@gmail.com>
2025-03-04 21:43:41 +0330

    wikiIcon fixed

--- wikiIcon/wikiIcon-plugin.js
+++ wikiIcon/wikiIcon-plugin.js
@@ -1,15 +1,16 @@
 window.$docsify = window.$docsify || {};
 window.$docsify.plugins = (window.$docsify.plugins || []).concat(function (hook) {
-  hook.beforeEach(function (content) {
-    return content.replace(/\[(wikipedia|ویکی‌پدیا)\]\((.*?)\)/g, function (match, label, url) {
-      const pageTitle = decodeURIComponent(url.split('/').pop()).replace('_', ' ');
+  hook.afterEach(function (content) {
+    return content.replace(/<a\s+href="([^"]+)"[^>]*>(wikipedia|ویکی‌پدیا)<\/a>/g, function (match, href) {
+      const pageTitle = decodeURIComponent(href.split('/').pop()).replace('_', ' ');
       return `
         <span class="wikipedia-preview-container">
-          <a href="${url}" target="_blank" class="wikipedia-link" title="${pageTitle}" data-page-href="${url}">
+          <a href="${href}" target="_blank" class="wikipedia-link" title="${pageTitle}" data-page-href="${href}">
             <img src="https://en.wikipedia.org/favicon.ico" alt="${pageTitle}" class="wikipedia-icon">
           </a>
           <span class="wikipedia-preview-box">
-            <iframe src="${url}" class="wikipedia-iframe"></iframe>
+            <span class="loading">بارگذاری ...</span>
+            <iframe src="${href}#bodyContent" class="wikipedia-iframe"></iframe>
           </span>
         </span>
       `;
